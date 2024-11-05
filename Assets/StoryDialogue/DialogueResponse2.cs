0c78a7f59a4947ba6d209af879006c6d54a0a7d6
WuLiuSeven <125783608+WuLiuSeven@users.noreply.github.com>
2024-11-04 19:55:19 -0500

    Added extra scene and story dialogue

---
+++ Assets/StoryDialogue/DialogueResponse2.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class DialogueResponse2// : MonoBehaviour
+{
+    public string responseText;
+    //initiate next conversation
+    public DialogueNode2 nextNode;
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
