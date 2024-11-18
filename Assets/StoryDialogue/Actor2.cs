46db8ff926e98576798715b9e11f9bbb41054cd3
WuLiuSeven <125783608+WuLiuSeven@users.noreply.github.com>
2024-11-18 12:13:24 -0500

    Dialogue Creation Method
    To create a dialogue:
    - create serializable script dialogue (this will make a dialogue script in the format of a tree). Add dialogue branch here
    - with the interactable NPC, add the Act2.cs script to it, you many add a title (name?) and add the serializable script dialogue to it
    Notes:
    - Z for interaction (checks for player in range)
    - to check for status before the dialog, go to the Update() function in the Actor2 script and add conditions to be checked
    - to update status after the interaction, go to SpeakTo() and add there conditions there (for individual interactions, check the tag of the NPC

--- Assets/StoryDialogue/Actor2.cs
+++ Assets/StoryDialogue/Actor2.cs
@@ -8,7 +8,7 @@ public class Actor2 : MonoBehaviour
     public string Name;
     public Dialogue2 Dialogue;
 
-    public void SpeakTo()
+    public void SpeakTo() //add additional effects using conditional statements here
     {
         DialogueManager2.instance.StartDialogue(Name, Dialogue.RootNode);
     }
@@ -19,13 +19,76 @@ public class Actor2 : MonoBehaviour
         
     }
     */
+
+    private bool isPlayerInRange = false;
+    // Detect when the player enters the NPC's interaction range
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+        }
+    }
+
+    // Detect when the player leaves the NPC's interaction range
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+        }
+    }
+
+    //checker to ensure that a NPC's dialogue does not run twice
+    private bool check = false;
+
     // Update is called once per frame
     private void Update()
     {
+        /*
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SpeakTo();
         }
+        */
+        //initiate dialogue after checking stats
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.Z) && check == false)
+        {
+
+            StatManager statManager = StatManager.Instance;
+            // testing, work; can access stats and change stats
+            if (statManager.Act1)
+            {
+                Debug.Log($"Act1 is true");
+            }
+            if (statManager.Act2)
+            {
+                Debug.Log($"Act2 is true");
+            }
+            if (!(statManager.Act1 = true))
+            {
+                Debug.Log($"Act1 is false");
+            }
+
+            if (!(statManager.Act2 = true))
+            {
+                Debug.Log($"Act2 is false changing it to true");
+                statManager.Act1 = true;
+            }
+            if ((statManager.Act2)) //should run over everything else
+            {
+                Debug.Log($"Act2 is true now");
+                SpeakTo();
+            }
+            if (!(statManager.Act2))
+            {
+                Debug.Log($"Act2 is still false, fix the error");
+            }
+
+            check = true;
+
+        }
+
     }
     
 }
