0714cd8895e361be6f1174f9adf7d7d94f0ab088
Edwin Gabriel <65310253+Edwinvilla72@users.noreply.github.com>
2024-11-20 19:45:02 -0500

    Battle Sounds
    Added:
    - Battle theme
    - Hit sound effect
    - Hovering over other options turns them different colors (attack = red, reason = green, etc.)

--- Assets/Scripts/MiniGameScripts/Bullet.cs
+++ Assets/Scripts/MiniGameScripts/Bullet.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-
-
 public class Projectile : MonoBehaviour
 {
-
     MinigameController minigameController;
+    AudioManager audioManager;
+
     // Start is called before the first frame update
     void Start()
     {
         minigameController = FindObjectOfType<MinigameController>();
+        audioManager = FindObjectOfType<AudioManager>(); // Find the AudioManager in the scene
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+<<<<<<< Updated upstream
         if (collision.gameObject.tag != "Bullet" && collision.gameObject.tag == "Wall")
         {
             
@@ -35,6 +35,31 @@ public class Projectile : MonoBehaviour
             Destroy(gameObject);
         }
         
+=======
+        if (collision.gameObject.tag != "Bullet")
+        {
+            if (collision.gameObject.tag == "Wall")
+            {
+                Destroy(gameObject);
+            }
+            else if (collision.gameObject.tag == "Player")
+            {
+                // Increment the counter in the MinigameController
+                minigameController.count++;
+
+                // Play the gotHit sound
+                if (audioManager != null && audioManager.gotHit != null)
+                {
+                    audioManager.PlaySFX(audioManager.gotHit);
+                }
+
+                // Destroy the projectile
+                Destroy(gameObject);
+
+                Debug.Log("hit");
+            }
+        }
+>>>>>>> Stashed changes
     }
-    
 }
+
